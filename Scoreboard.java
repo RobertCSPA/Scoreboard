02a7645c8f82191712b95d63b9bcd6523964554d
RobertCSPA <robertf127@nycstudents.net>
2024-09-27 16:21:43 +0000

    add scores

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,1 +1,19 @@
+public class Scoreboard
+{
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+    private String teamOne;
+    private String teamTwo;
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
 
+}
